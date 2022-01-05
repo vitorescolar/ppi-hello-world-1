0a760e88d6877713a25ba6f8c2dd3de5f6f8b70d
Vitor Devidy Costa <v.devidy@academico.ifrn.edu.br>
2022-01-05 07:55:43 -0300

    afjsaggaj

--- index.php
+++ index.php
@@ -8,5 +8,12 @@
 </head>
 <body>
     <h1>Marcelo Júnior</h1>
+    <?php
+        require_once "model/Conexao.php";
+        $tecnologias = Conexao::select(
+            "tecnologia", "id, nome"
+        );
+        print_r($tecnologias);
+    ?>
 </body>
 </html>
